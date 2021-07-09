3f738533069d4736947e744d3d7d3930714a5135
rakhimgaliyevna <77445559+rakhimgaliyevna@users.noreply.github.com>
2021-07-09 19:38:56 +0600

    artist songs sending added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express= require("express");
+const express = require("express");
 
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require("fs");
@@ -8,26 +8,26 @@ const token = '1814848685:AAEEiaj6WyHySeoNL3-z3iPOLpuD-Rlmr_U';
 const bot = new TelegramBot(token, { polling: true });
 
 var weather = require('weather-js');
- 
+
 let app = express();
 
 app.set("view engine", "ejs")   //говорим что используем движок еджс
 
-const filePath  = "./db/users.txt";
+const filePath = "./db/users.txt";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-const checkUnique = (chatId) =>{
-    if(fs.existsSync(filePath)){
+const checkUnique = (chatId) => {
+    if (fs.existsSync(filePath)) {
         let users = fs.readFileSync(filePath);
         users = JSON.parse(users);
         let unique = true;
 
-        for(let i = 0; i < users.length; i++){
-            if(users[i].chatId === chatId){
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].chatId === chatId) {
                 unique = false;
                 break;
             }
@@ -41,42 +41,42 @@ const saveUser = (username, chatId) => {
         username: username,
         chatId: chatId
     }
-    if(checkUnique(chatId)){
+    if (checkUnique(chatId)) {
         let users = null;
 
-        try{
-            users = fs.readFileSync(filePath); 
+        try {
+            users = fs.readFileSync(filePath);
             users = JSON.parse(users)
-            
-        }catch{
+
+        } catch {
             users = [];
         }
 
         //bot.sendMessage(chatId, `${user.username}, ${user.chatId}`);
         users.push(user);
 
-        fs.writeFileSync(filePath,JSON.stringify(users));
+        fs.writeFileSync(filePath, JSON.stringify(users));
 
         bot.sendMessage(chatId, "Вы успешно зарегистрировались!");
-    }else{
+    } else {
         bot.sendMessage(chatId, "Вы уже зарегистрировались!");
     }
 }
 
-app.get("/contacts", function(request, response){
+app.get("/contacts", function (request, response) {
     //saveUser("dinara test", 275156835)
     response.render("contacts.ejs", {
         title: "Страница контактов",
         contacts: JSON.parse(fs.readFileSync(filePath))
-        
+
     });
 })
-app.post("/bot/message", function(request, response){
+app.post("/bot/message", function (request, response) {
     let chatId = request.body.chatId;
     bot.sendMessage(chatId, "Вам отправили сообщение из сайта <3<3<3")
 })
 
-bot.onText(/\/register/, function(msg, match){  //команда в боте
+bot.onText(/\/register/, function (msg, match) {  //команда в боте
     let textArray = msg.text.split(" ");     //===match[1];//разделить
     textArray.splice(0, 1);     //удаление первого элемента
     let textWithoutCommand = textArray.join(" ");   //соединить
@@ -85,21 +85,58 @@ bot.onText(/\/register/, function(msg, match){  //команда в боте
     bot.sendMessage(msg.chat.id, "Команда регистации \n с текстом " + textWithoutCommand);
 })
 
-bot.onText(/\/weather/, function(msg, match){   //создание команды в боте
-    
-    
-    
+bot.onText(/\/weather/, function (msg, match) {   //создание команды в боте
+
+
+
     let city = msg.text.split(" ")[1];     //===match[1];//разделить
 
-    weather.find({search: city, degreeType: 'C'}, function(err, result) {
-        if(err) console.log(err);
-       
+    weather.find({ search: city, degreeType: 'C' }, function (err, result) {
+        if (err) console.log(err);
+
         bot.sendMessage(msg.chat.id, `Погода в ${city}` + ":" + result[0]['current']['temperature']);
     });
 
 })
+bot.onText(/\/start/, (msg) => {
+    let files = fs.readdirSync("./media/audio");
+    let messageArray = fullMessage.split(" ");
+    messageArray.splice(0, 1);
+    let messageWithoutCommand = messageArray.join(" ");
 
-bot.on("message", function(msg){
-    saveUser(msg.chat.username, msg.chat.id);
+    let artistSongs = [];
+
+    for(let i=0;i < files.length; i++){
+        if(files[i].includes(messageWithoutCommand))    //includes-содержиться
+        artistSongs.push(files[i]);
+    }
+    bot.sendMessage(msg.chat.id, "Проверьте клавиатуру", {
+        "reply_markup": {
+            "keyboard": [artistSongs]
+        }
+    });
+
+});
+bot.on("message", function (msg) {
+    let messageText = msg.text;
+    bot.sendAudio(msg.chat.id, "./media/audio/" + messageText)
+
+    // saveUser(msg.chat.username, msg.chat.id);
+    // if(msg.text.toLowerCase() === "зарегистрироваться"){
+    //     spamUser();
+    // }
+    
+    // if(msg.text.toLowerCase() === "miyagi"){
+    //     bot.sendAudio(msg.chat.id, "./media/audio/miyagi.mp3")
+    // }
+    // if(msg.text.toLowerCase() === "москва"){
+    //     bot.sendAudio(msg.chat.id, "./media/audio/moscow.mp3")
+    // }
+    // if(msg.text.toLowerCase() === "вечеринка"){
+    //     bot.sendAudio(msg.chat.id, "./media/audio/vecherinka.mp3")
+    // }
+    
 })
-app.listen(8080);
+let port = process.env.PORT === undefined ? 3030 : process.env.PORT;
+
+app.listen(port);
